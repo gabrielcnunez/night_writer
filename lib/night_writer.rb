e452ee965c02a5db35f2636626f1550043b2abc0
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-09-22 22:29:56 -0700

    Add argv interpolation to puts stmt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,9 @@
-puts "Created 'braille.txt' containing 256 characters"
+message = File.open(ARGV[0], "r")
+
+incoming_text = message.read
+
+
+message.close
+
+
+puts "Created " + ARGV[1] + " containing 256 characters"
