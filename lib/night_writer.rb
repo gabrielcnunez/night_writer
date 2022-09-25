c8e065cf13a933d07f1da44d4572c9468b626678
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-09-25 12:56:44 -0700

    Create initialize method, pass test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,13 @@
+class NightWriter
+  attr_reader :incoming_text,
+              :translate_to_braille
+              
+  def initialize(incoming_text, translate_to_braille)
+    @incoming_text = incoming_text
+    @translate_to_braille = translate_to_braille
+  end
+end
+
 message = File.open(ARGV[0], "r")
 
 incoming_text = message.read
@@ -8,4 +18,4 @@ total_characters = text.size
 message.close
 
 
-puts "Created " + ARGV[1] + " containing #{total_characters} characters"
+# puts "Created " + ARGV[1] + " containing #{total_characters} characters"
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 RSpec.describe NightWriter do
   describe '#initialize' do
     it 'exists' do
-    
+      night_writer = NightWriter.new("message.txt", "braille.txt")
+
+      expect(night_writer).to be_an_instance_of(NightWriter)
     end
   end
 
