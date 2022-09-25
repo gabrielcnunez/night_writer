a3ff5e3dab88b27030107c46fa5d86f5841e3d6a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-09-25 11:52:43 -0700

    Add test framework from i1 command line setup

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,2 +1,19 @@
 require 'spec_helper'
 
+RSpec.describe NightWriter do
+  describe '#initialize' do
+    it 'exists' do
+    
+    end
+  end
+
+  describe '#print_reply' do
+    it 'can print static reply' do
+    end
+
+    it 'can print dynamic reply based on character numbers' do
+
+    end
+  end
+  
+end
