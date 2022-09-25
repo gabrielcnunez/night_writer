d5c60144b87f49fc336d780df79e8f4aebb0065b
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-09-25 15:04:20 -0700

    Create hello_world.txt file, update tests

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe NightWriter do
   describe '#initialize' do
     it 'exists' do
-      night_writer = NightWriter.new("message.txt", "braille.txt")
+      night_writer = NightWriter.new("hello_world.txt", "braille.txt")
 
       expect(night_writer).to be_an_instance_of(NightWriter)
     end
@@ -11,10 +11,18 @@ RSpec.describe NightWriter do
 
   describe '#print_reply' do
     it 'can print a reply based on character numbers' do
-      night_writer = NightWriter.new("message.txt", "braille.txt")
+      night_writer = NightWriter.new("hello_world.txt", "braille.txt")
 
       expect(night_writer.print_reply).to eq("Created 'braille.txt' containing 11 characters")
     end
   end
   
+  describe '#write_to_file' do
+    # wip
+    xit 'can write a new file' do
+      night_writer = NightWriter.new("hello_world.txt", "braille.txt")
+
+      expect
+    end
+  end
 end
