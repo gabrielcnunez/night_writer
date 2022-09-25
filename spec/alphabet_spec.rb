5f9fc898ebdf26b3bd3acb98cacd9e3342a8ef6f
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-09-25 16:14:36 -0700

    Create initialize tests for alphabet class

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -1,1 +1,17 @@
 require 'spec_helper'
+
+RSpec.describe Alphabet do
+  describe '#initialize' do
+    it 'exists' do
+      alphabet = Alphabet.new
+
+      expect(alphabet).to be_an_instance_of(Alphabet)
+    end
+
+    it 'has readable attributes' do
+      alphabet = Alphabet.new
+
+      expect(alphabet["h"]).to eq(["0.", "00", ".."])
+    end
+  end
+end
