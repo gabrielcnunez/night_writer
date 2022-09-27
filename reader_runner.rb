d8407732cdbb09701057b30bc5aa21497120e699
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-09-27 14:22:48 -0700

    Update runner to run on command line

--- reader_runner.rb
+++ reader_runner.rb
@@ -1,6 +1,8 @@
-require './lib/write_reader'
+require './lib/night_reader'
 
 incoming_braille = ARGV[0]
 outgoing_text = ARGV[1]
 
 night_reader = NightReader.new(incoming_braille, outgoing_text)
+night_reader.write_to_file
+night_reader.print_reply
